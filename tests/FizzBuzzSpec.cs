94ee10dfa323f23c3f377f2da2e7d5df920b5094
Johannes Prinz <johannesprinz@gmail.com>
2020-10-28 09:08:51 +0000

    Lets start on the first kata

---
+++ tests/FizzBuzzSpec.cs
@@ -0,0 +1,21 @@
+using TDDDojo.Domain;
+using FluentAssertions;
+using Xunit;
+using System;
+
+namespace tests
+{
+    public class FizzBuzzSpec
+    {
+        // FizzBuzz is to print out number 1 to 100
+        // replacing numbers divisible by 3 with Fizz
+        // replacing numbers divisible by 5 with Buzz
+        // replace numbers divicible by 3 and 5 with FizzBuzz
+
+        [Fact]
+        public void Lets_get_started()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
